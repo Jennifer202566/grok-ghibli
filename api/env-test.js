5fc09d2ac039297c6d8b1dc400ec656783ef40ef
Jennifer202566 <amybluebeads@gmail.com>
2025-04-21 21:49:34 +0800

    Update env-test.js

--- api/env-test.js
+++ api/env-test.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
   
   // 只显示密钥的前几个字符，保护安全
   const maskedKey = apiKey === 'not set' ? 'not set' : 
-                    apiKey.substring(0, 4) + '...' + apiKey.substring(apiKey.length - 4);
+                   apiKey.substring(0, 4) + '...' + apiKey.substring(apiKey.length - 4);
   
   res.status(200).json({
     message: 'Environment variable test',
